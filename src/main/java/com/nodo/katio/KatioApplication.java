0978a916dfbec29828da162796915594ae688ea2
SaraCastril1 <smcastril1@eafit.edu.co>
2024-04-01 11:32:36 -0500

    feat: scaffolding

---
+++ src/main/java/com/nodo/katio/KatioApplication.java
@@ -0,0 +1,13 @@
+package com.nodo.katio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nodo/katio/KatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodo.katio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c7ea7ffe5b6014d8deba9838e8ee133131bf6c82
Ronycore <127463614+Ronycore@users.noreply.github.com>
2024-05-04 10:49:21 -0500

    Se hacen cambios para corregir atributos que estaban mal ingresados.

--- src/main/java/com/nodo/katio/models/Authorrepositories.java
+++
@@ -1,8 +0,0 @@
-package com.nodo.katio.models;
-
-public class Authorrepositories {
-    private String NombreDelAutor;
-    private String ApellidoDelAutor;
-    private String LugarDeNacimientoDelAutor;
-    private String GeneroDelAutor;
-}
--- src/main/java/com/nodo/katio/services/AuthorService.java
+++ src/main/java/com/nodo/katio/services/AuthorService.java
@@ -1,73 +1,75 @@
 
 package com.nodo.katio.services;
 
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.stereotype.Service;
 import com.nodo.katio.repositories.AuthorRepository;
+import com.nodo.katio.interfaces.BaseAuthorService;
 import com.nodo.katio.models.Author;
 
-@Service
-public class AuthorService implements AuthorService {
+public class AuthorService implements BaseAuthorService {
 
-    @SuppressWarnings({ "rawtypes", "unused" })
     private AuthorRepository AuthorRepository;
 
-    public AuthorService(@SuppressWarnings("rawtypes") AuthorRepository userRepository) {
+    public AuthorService(AuthorRepository userRepository) {
         this.AuthorRepository = AuthorRepository;
     }
+    /*
+     * @Override
+     * public Iterable<BaseAuthorService> getAllUsers() {
+     * return AuthorRepository.findAll();
+     * }
+     * 
+     * @Override
+     * public Author Author(Author Author) {
+     * try {
+     * // if(user.getPasshash().length() > 15)
+     * // {
+     * // user.setPasshash(blake3Formatter(user.getPasshash()));
+     * // user = userRepository.saveAndFlush(user);
+     * // }
+     * Author.setPasshash(blake3Formatter(Author.getPasshash()));
+     * Author = (com.nodo.katio.models.Author)
+     * AuthorRepository.saveAndFlush(Author);
+     * 
+     * } catch (Exception ex) {
+     * ex.printStackTrace();
+     * }
+     * return Author;
+     * }
+     * 
+     * // Encriptación en una sola dirección
+     * private String blake3Formatter(String value) throws NoSuchAlgorithmException
+     * {
+     * final MessageDigest md = MessageDigest.getInstance("SHA3-512");
+     * byte[] hash = md.digest(value.getBytes(StandardCharsets.UTF_8));
+     * String sha3Hex = bytesToHex(hash);
+     * return sha3Hex;
+     * }
+     * 
+     * private String bytesToHex(byte[] hash) {
+     * BigInteger number = new BigInteger(1, hash);
+     * StringBuilder hexString = new StringBuilder(number.toString(16));
+     * while (hexString.length() < 64) {
+     * hexString.insert(0, '0');
+     * }
+     * return hexString.toString();
+     * }
+     * 
+     * @Override
+     * public <User> User addAutorService(com.nodo.katio.models.Author Author) {
+     * // TODO Auto-generated method stub
+     * throw new
+     * UnsupportedOperationException("Unimplemented method 'addAutorService'");
+     * }
+     */
 
-    @SuppressWarnings("unchecked")
     @Override
     public Iterable<Author> getAllUsers() {
         return AuthorRepository.findAll();
     }
 
     @Override
-    public Author Author(Author Author) {
-        try {
-            // if(user.getPasshash().length() > 15)
-            // {
-            // user.setPasshash(blake3Formatter(user.getPasshash()));
-            // user = userRepository.saveAndFlush(user);
-            // }
-            Author.setPasshash(blake3Formatter(Author.getPasshash()));
-            Author = (com.nodo.katio.models.Author) AuthorRepository.saveAndFlush(Author);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return Author;
+    public Author addAutorService(Author author) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addAutorService'");
     }
-
-    // Encriptación en una sola dirección
-    private String blake3Formatter(String value) throws NoSuchAlgorithmException {
-        final MessageDigest md = MessageDigest.getInstance("SHA3-512");
-        byte[] hash = md.digest(value.getBytes(StandardCharsets.UTF_8));
-        String sha3Hex = bytesToHex(hash);
-        return sha3Hex;
-    }
-
-    private String bytesToHex(byte[] hash) {
-        BigInteger number = new BigInteger(1, hash);
-        StringBuilder hexString = new StringBuilder(number.toString(16));
-        while (hexString.length() < 64) {
-            hexString.insert(0, '0');
-        }
-        return hexString.toString();
-    }
-
-    @SuppressWarnings("rawtypes")
-    public AuthorRepository getAuthorRepository() {
-        return AuthorRepository;
-    }
-
-    public void setAuthorRepository(AuthorRepository authorRepository) {
-        AuthorRepository = authorRepository;
-    }
-
 }
